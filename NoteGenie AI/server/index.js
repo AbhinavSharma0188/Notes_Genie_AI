4be4f96e84461fb112f21171b80f1becb5f062d0
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-03-26 18:04:52 +0530

    login functionality done

--- NoteGenie AI/server/index.js
+++ NoteGenie AI/server/index.js
@@ -1,12 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./utils/connectDB.js";
+import authRouter from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
 const app=express();
 const PORT=process.env.PORT || 3000;
 app.get("/",(req,res)=>{
     res.send("Hello world");
 })
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+    methods:["GET","POST","PUT","DELETE","OPTIONS"],
+    allowedHeaders:["Content-Type","Authorization"],
+})  )
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth",authRouter);
 app.listen(PORT,()=>{
     console.log(`Server is listening to the port ${PORT}`)
     connectDB();
---
+++ NoteGenie AI/server/routes/auth.routes.js
@@ -0,0 +1,5 @@
+import express from "express";
+import { googleAuth } from "../controllers/auth.controller.js";
+const authRouter=express.Router();
+authRouter.post("/google",googleAuth);
+export default authRouter;  
