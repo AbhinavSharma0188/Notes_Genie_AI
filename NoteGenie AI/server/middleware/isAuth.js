44455c3dc06157aac07c25bbe008a4f4eb5d5c93
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-03-28 08:44:19 +0530

    isAuth middleware

---
+++ NoteGenie AI/server/middleware/isAuth.js
@@ -0,0 +1,29 @@
+const isAuth=async(req,res,next)=>{
+    try {
+        const token=req.cookies;
+        if(!token){
+            return res.status(401).json({
+                success:false,
+                message:"Unauthorized"
+            })
+        }
+        const verifyToken=jwt.verify(token,process.env.JWT_SECRET);
+        if(!verifyToken){
+            return res.status(401).json({
+                success:false,
+                message:"Unauthorized"
+            })
+        }
+        req.userId=verifyToken.userId;
+        next();
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:"Internal server error"
+        })
+    }
+
+}
+export default isAuth;
