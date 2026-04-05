4e326fa27a307aba23f8abe0323649f954702240
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-04-06 01:05:39 +0530

    lets design home page

---
+++ NoteGenie AI/client/vite-project/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='relative z-20 mx-6 mt-6 rounded-2xl bg-gradient-to-br from-black/90 via-black/80 to-black/90 backdrop-blur-2xl border border-white/10 shadow-[0_22px_55px_rgba(0,0,0,0,75)] flex items-center justify-between px-8 py-4'>
+        
+        Navbar
+    </div>
+   
+  )
+}
+
+export default Navbar
